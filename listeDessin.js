e41e4fab81462311640a9a30a6c291976aa443e4
Jérémy Lorette <jeremy.lorette@outlook.com>
2021-06-11 10:39:24 +0200

    Create listeDessin.js

---
+++ listeDessin.js
@@ -0,0 +1,35 @@
+class Galerie {
+
+    constructor(title,date,type) {
+
+        this.title = title;
+
+        this.date = date; 
+
+        this.type = type;
+    }
+}
+
+let spiderMan = new Galerie("spider-man",2020,"digital");
+
+let captainAmérica = new Galerie("captainAmérica",2020,"digital");
+
+let ironMan = new Galerie("iron-man",2020,"digital");
+
+let radje = new Galerie("radje",2020,"digital");
+
+let howard = new Galerie("howard",2020,"digital")
+
+let léonard = new Galerie("léonard",2020,"digital");
+
+let sheldon = new Galerie("sheldon",2020,"digital");
+
+let spiderMan2 = new Galerie("spider man 2",2020,"digital");
+
+let harryPotter = new Galerie("Harry potter",2021,"digital");
+
+let wormav = new Galerie("wormav",2021,"logo");
+
+let sasum = new Galerie("sasum",2021,"logo");
+
+let yukira = new Galerie("yukira",2021,"Logo");
